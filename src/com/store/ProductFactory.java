356bcb9589c8c864476aed67770cbd703936624b
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-19 17:07:49 +0500

    Update ProductFactory.java

--- src/com/store/ProductFactory.java
+++ src/com/store/ProductFactory.java
@@ -1,4 +1,17 @@
-package com.store;
+package com.store.factory;
+
+import com.store.product.Product;
 
 public class ProductFactory {
+
+    public static Product createProduct(String type) {
+
+        Product product = new Product();
+
+        if (type.equals("default")) {
+            product = new Product();
+        }
+
+        return product;
+    }
 }
