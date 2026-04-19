906066ed2963bfbfcf8dde56759bfef562a9933c
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-19 17:02:53 +0500

    Update Order.java

--- src/com/store/Order.java
+++ src/com/store/Order.java
@@ -1,4 +1,17 @@
-package com.store;
+package com.store.order;
+
+import java.util.List;
+import com.store.product.Product;
+import com.store.user.Customer;
 
 public class Order {
+    private Long id;
+    private Customer customer;
+    private List<Product> products;
+    private double total;
+    private String status;
+
+    public void createOrder() {}
+    public void cancel() {}
+    public void pay() {}
 }
