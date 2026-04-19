99329d0acc47eaee2eccec0a7d13745087fd85af
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-19 17:06:53 +0500

    Update Review.java

--- src/com/store/Review.java
+++ src/com/store/Review.java
@@ -1,4 +1,10 @@
-package com.store;
+package com.store.review;
+
+import com.store.product.Product;
 
 public class Review {
+
+    private Product product;
+    private String comment;
+    private int rating;
 }
