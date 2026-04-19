a9434f7ed40bc648a7f298c072a5e396678abf3b
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-19 17:01:30 +0500

    Update Customer.java

--- src/com/store/Customer.java
+++ src/com/store/Customer.java
@@ -1,4 +1,12 @@
-package com.store;
+package com.store.user;
 
-public class Customer {
+import java.util.List;
+import com.store.order.Order;
+
+public class Customer extends User {
+    private List<Order> orders;
+    private int bonusPoints;
+
+    public void register() {}
+    public void updateData() {}
 }
