2694192490cd663020781910bccc08e527f49e9f
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-19 17:03:39 +0500

    Update Payment.java

--- src/com/store/Payment.java
+++ src/com/store/Payment.java
@@ -1,4 +1,11 @@
-package com.store;
+package com.store.payment;
 
 public class Payment {
+    private Long id;
+    private String type;
+    private double amount;
+    private String status;
+
+    public void process() {}
+    public void refund() {}
 }
