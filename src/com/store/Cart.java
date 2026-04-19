7330558175a058078bc0d790567a5e94535814d2
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-19 17:03:20 +0500

    Update Cart.java

--- src/com/store/Cart.java
+++ src/com/store/Cart.java
@@ -1,4 +1,11 @@
-package com.store;
+package com.store.order;
+
+import java.util.List;
+import com.store.product.Product;
 
 public class Cart {
+    private List<Product> items;
+
+    public void addProduct(Product product) {}
+    public void removeProduct(Product product) {}
 }
